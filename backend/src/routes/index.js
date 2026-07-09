35e27ecad721039a6ce596548e5dcdc081f5a84a
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-07-09 07:17:19 +0600

    added the routes in router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
+const userRoutes = require("../modules/user/user.route");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "WarrantyWallet API v1",
-    });
-});
+router.use("/users", userRoutes);
 
 module.exports = router;
